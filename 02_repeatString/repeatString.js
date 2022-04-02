9e50415363ab823d96e569dca293099821f8baae
BZIEO <100628007+BZIEO@users.noreply.github.com>
2022-04-02 15:48:04 -0500

    repeatString test & debug

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let rStr = "";
+  if (num >= 0) {
+    while (num > 0) {
+      rStr += str;
+      num--;
+    }
+    return rStr;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
