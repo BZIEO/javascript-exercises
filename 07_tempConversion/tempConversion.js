e1b26ca2b98de235604cbc5a8cb8563afe6a75df
BZIEO <100628007+BZIEO@users.noreply.github.com>
2022-04-02 19:56:06 -0500

    tempConversion test & debug

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
-const ftoc = function() {
+const ftoc = function (x) {
+  let c = (x - 32) * (5 / 9);
+  c = Math.round(c * 10) / 10;
 
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function (y) {
+  let f = y * 1.8 + 32;
+  f = Math.round(f * 10) / 10;
+  return f;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
