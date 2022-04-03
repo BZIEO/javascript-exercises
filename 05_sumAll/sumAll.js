1cc4f56eb544e8ee77b9896dd69eb9e20605aa03
BZIEO <100628007+BZIEO@users.noreply.github.com>
2022-04-02 19:16:41 -0500

    sumAll test & debug

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  } else if (a < 0 || b < 0) {
+    return "ERROR";
+  } else if (a < b) {
+    for (i = a; i <= b; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  } else if (a > b) {
+    for (i = b; i <= a; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
